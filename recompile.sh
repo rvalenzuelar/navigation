694ae2159d3107efbd8d93655a8bfb9e760ca583
rvalenzuela <raul.valenzuela@colorado.edu>
2015-03-20 08:35:42 -0600

    compile fortran and cpp code

--- recompile.sh
+++ recompile.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
+
 # Bash script to compile the navigation correction code
 
-rm *.o aster2txt cns_noaa-p3_rv netcdf2text 
-make
-echo
+rm aster2txt cns_noaa-p3_rv netcdf2text 
+
+NETCDF="/usr/local"
+
+FLIB="-L$NETCDF/lib -lnetcdf -lnetcdff -lcurl -lhdf5 -lhdf5_hl"
+
+FINC="-I$NETCDF/include -I/usr/include/geotiff"
+
+CLIB="-L/usr/lib -lgeotiff -ltiff -L/usr/local/lib"
+
+CINC="-I/usr/include/geotiff"
+
+gfortran -g -o cns_noaa-p3_rv cns_noaa-p3_rv.f90 chol_inv.f $FLIB $FINC
+
+gfortran -g -o netcdf2text netcdf2text.f90 $FLIB $FINC
+
+gcc -o aster2txt aster2txt.c $CLIB $CINC
+
