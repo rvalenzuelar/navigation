f6ef4cab1a1154886e9de431175ddf2b102571a3
rvalenzuelar <raul.valenzuela@colorado.edu>
2015-05-11 12:05:13 -0600

    netcdf2text moved to new repository

--- recompile.sh
+++ recompile.sh
@@ -3,7 +3,8 @@
 # Bash script to compile the navigation correction code
 
 # rm aster2txt cns_noaa-p3_rv netcdf2text 
-rm aster2txt cns_eldo_cai netcdf2text
+# rm aster2txt cns_eldo_cai netcdf2text
+rm aster2txt cns_eldo_cai
 
 NETCDF="/usr/local"
 
@@ -22,7 +23,7 @@ CINC="-I/usr/include/geotiff"
 # gfortran -g -o cns_noaa-p3_rv cns_noaa-p3_rv.f90 chol_inv.f $FLIB $FINC
 gfortran -g -o cns_eldo_cai cns_eldo_cai.f chol_inv.f $FLIB $FINC
 
-gfortran -g -o netcdf2text netcdf2text.f90 $FLIB $FINC
+# gfortran -g -o netcdf2text netcdf2text.f90 $FLIB $FINC
 
 gcc -o aster2txt aster2txt.c $CLIB $CINC
 
