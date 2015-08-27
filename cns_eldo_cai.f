745607d2209179e5afd624b8d70c224cc9539592
rvalenzuelar <raul.valenzuela@colorado.edu>
2015-08-27 11:07:37 -0600

    larger buffer size (4400) for dtm

--- cns_eldo_cai.f
+++ cns_eldo_cai.f
@@ -29,7 +29,8 @@ c******************************************************************
 
       program cns_eldo
 c
-      parameter (nvar=12,nxysurfmax=1900)
+      ! parameter (nvar=12,nxysurfmax=1900)
+      parameter (nvar=12,nxysurfmax=4400)
 c
 c     include '/home/users/rouf/SOURCES/ELDO/mes_parametres'
 c CAI-START: Inlcude the parameter file mes_parametres directly below
@@ -556,7 +557,7 @@ c CAI STOP
 	altdtm_max=-999.
         do jdtm=1,ny_dtm
 	   read(20,333)(itab(idtm),idtm=1,nx_dtm)
-  333      format(1900i6)
+  333      format(4400i6)
       	   do idtm=1,nx_dtm
       	      if(itab(idtm).gt.-1000)then
       		h_dtm=float(itab(idtm))/1000.
